213f05105fdfa952bed29ee51e69d9f025224b5a
Srikanth <kksrikanth@gmail.com>
2020-10-15 17:39:37 +1100

    Added pact testing for provider.

--- src/test/java/com/ing/assignment/user/UserControllerProviderTest.java
+++ src/test/java/com/ing/assignment/user/UserControllerProviderTest.java
@@ -8,6 +8,7 @@ import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import com.ing.assignment.user.model.Address;
 import com.ing.assignment.user.model.User;
 import com.ing.assignment.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,11 +45,13 @@ public class UserControllerProviderTest {
 
     @State({"provider accepts a new user",
             "person 11 exists"})
-    public void toCreatePersonState() {
-        User user = new User();
-        user.setId(11L);
-        user.setFirstName("Bill");
-        user.setLastName("Clinton");
+    public void toCreateUserState() {
+        User user = User.builder()
+                .id(11L)
+                .firstName("Bill").lastName("Clinton").emailId("george.washington@email.com").gender("Male").title("Mr")
+                .address(Address.builder().street("Pennsylvania Avenue").city("NW").state("Washington D.C").postCode("2000").build())
+                .build();
+
         when(userRepository.findById(eq(11L))).thenReturn(Optional.of(user));
         when(userRepository.save(any(User.class))).thenReturn(user);
     }
